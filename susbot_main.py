dc152e78fe9f23e5c5a2aca756911c099c0147ab
JustCallMeSimon <no>
2021-11-05 19:50:46 +0100

    -Refactored Color System and loading -Adapted Color System in rainbowbrush -Implemented a Bot Class (Basic) -Slightly changed login method -Refactored Control printing into a method

--- susbot_main.py
+++ susbot_main.py
@@ -2,7 +2,7 @@
 #feel free to change it to suit your needs *you do not have permission to sell any code derived from this code: GNU Affero General Public License v3.0
 
 #this first import is where you store your username and password to login to Reddit(required), found in: crewmate.py
-import crewmate
+from crewmate import username, password
 
 import random
 import time
@@ -17,169 +17,98 @@ import pyautogui as autogui
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome() #you need chromedriver.exe: https://chromedriver.chromium.org/downloads
+class Bot(): 
+    def __init__ (self,username,password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Chrome() #you need chromedriver.exe: https://chromedriver.chromium.org/downloads
+        self.login()
+        self.colors = self.get_colors()
+
+    def login(self):
+        #login sequence, using Reddit, set your username and pass for Reddit in crewmate.py's variables 'username' and 'password'
+        print ('Logging in now through Reddit. If you get an error you may need to set your name and password in crewmate.py')
+        self.driver.get("https://pixelplace.io/api/sso.php?type=2&action=login")
+        self.driver.find_element_by_id('loginUsername').send_keys(self.username)
+        self.driver.find_element_by_id('loginPassword').send_keys(self.password)
+        self.driver.find_elements_by_xpath('/html/body/div/main/div[1]/div/div[2]/form/fieldset')[4].click()
+
+        #Simon - I suggest waiting on the button that needs to be pressed to appear instead of waiting a fixed time
+        
+        time.sleep(7)#adjust this sleep in seconds if logging after sending name/pass is slow for you
+
+        self.driver.find_elements_by_xpath('/html/body/div[3]/div/div[2]/form/div/input')[0].click()
+        print ('All done logging in, ready to paint.')
+        #end login
+
+    #Simon - Refactored Colors into getting saved into a dictionary via a for loop. Also renamed cycles according to Keystrokes
+
+    def get_colors(self): 
+            #setting up the colors for fast color switching:
+            self.color_dict = {}
+            for color_name in ["white","grey1","grey2","grey3","grey4","black","green1","green2","green3","green5","yellow1","yellow2","yellow3","yellow4","brown1","brown2","brown3","red1","red2","red3","brown4","peach1","peach2","peach3","pink1","pink2","pink3","pink4","blue1","blue2","blue3","blue4","blue5","blue6","blue7"],x in range(0,37):
+                path = self.driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[x]
+                self.color_dict.update({f"colorname":path})
+            self.Colors_W =  cycle([self.color_dict.get("grey4"), self.color_dict.get("grey3"), self.color_dict.get("grey2"), self.color_dict.get("grey1")])
+            self.Colors_A =  cycle([self.color_dict.get("green1"), self.color_dict.get("green2"), self.color_dict.get("green3"), self.color_dict.get("green5")])
+            self.Colors_S =  cycle([self.color_dict.get("red1"), self.color_dict.get("red2"), self.color_dict.get("red3")])
+            self.Colors_D =  cycle([self.color_dict.get("brown1"), self.color_dict.get("brown2"), self.color_dict.get("brown3"), self.color_dict.get("brown4"), self.color_dict.get("grey4"), self.color_dict.get("peach3"), self.color_dict.get("white")])
+            self.Colors_AW = cycle([self.color_dict.get("yellow1"), self.color_dict.get("yellow2"), self.color_dict.get("yellow3"), self.color_dict.get("yellow4")])
+            self.Colors_WD = cycle([self.color_dict.get("brown1"), self.color_dict.get("brown2"), self.color_dict.get("brown3"), self.color_dict.get("brown4")])
+            self.Colors_DS = cycle([self.color_dict.get("black"), self.color_dict.get("white")])
+            self.Colors_SA = cycle([self.color_dict.get("blue1"),self.color_dict.get("blue2"),self.color_dict.get("blue3"),self.color_dict.get("blue4"),self.color_dict.get("blue5"),self.color_dict.get("blue6"),self.color_dict.get("blue7")])
+
+    #Simon - Adapted the new Color System and compacted it a bit
+
+    #this is how the key combos work, for example if you press A and W it will run whichcolor2() aka the yellow colors
+    def rainbowbrush(self): 
+        if keyboard.is_pressed("s") or keyboard.is_pressed("a") or keyboard.is_pressed("w") == True or keyboard.is_pressed("d") == True:        
+            #only A
+            if keyboard.is_pressed("a") == True and keyboard.is_pressed("s") != True and keyboard.is_pressed("w") != True and keyboard.is_pressed("d") != True:
+                next(self.Colors_A).click()
+            #only S
+            if keyboard.is_pressed("s") == True and keyboard.is_pressed("a") != True and keyboard.is_pressed("w") != True and keyboard.is_pressed("d") != True:
+                next(self.Colors_S).click()
+            #only AW
+            if keyboard.is_pressed("a") == True and keyboard.is_pressed("w") == True and keyboard.is_pressed("d") != True and keyboard.is_pressed("s") != True:
+                next(self.Colors_AW).click()
+            #only W
+            if keyboard.is_pressed("w") == True and keyboard.is_pressed("a") != True and keyboard.is_pressed("s") != True and keyboard.is_pressed("d") != True:
+                next(self.Colors_W).click()
+            #only WD
+            if keyboard.is_pressed("w") == True and keyboard.is_pressed("d") == True and keyboard.is_pressed("s") != True and keyboard.is_pressed("a") != True:
+                next(self.Colors_WD).click()
+            #only D
+            if keyboard.is_pressed("d") == True and keyboard.is_pressed("s") != True and keyboard.is_pressed("a") != True and keyboard.is_pressed("w") != True:
+                next(self.Colors_D).click()
+            #only DS
+            if keyboard.is_pressed("d") == True and keyboard.is_pressed("s") == True and keyboard.is_pressed("a") != True and keyboard.is_pressed("w") != True:
+                next(self.Colors_DS).click()
+            #only SA
+            if keyboard.is_pressed("s") == True and keyboard.is_pressed("a") == True and keyboard.is_pressed("d") != True and keyboard.is_pressed("w") != True:
+                next(self.Colors_SA).click()
+
+def print_Controls():
+    print ("""
+    -Controls-')
+    Q: Cycle through all colors.
+    WASD: cycles different color palletes depending on which combination you use, for example W + A is yellows.
+    E: Draw an Among Us character, make sure your zoom level is 1 on pixelplace. Note: it only works when you are moving your mouse cursor slighty when you press it.
+    For random drawing, press Y on the top left of your zone, and U on the bottom left then use either R to draw random trees or K to draw random Among Us characters.
+    J: hold J to stop drawing random trees or Among Us characters.
+    ESC: hold Escape to halt the script.
+    """)
+
 
 #some vars for later:
 z, vector = 1, 0
 thecolor = None
 tX, bX, tY, bY = None, None, None, None  
 
-#login sequence, using Reddit, set your username and pass for Reddit in crewmate.py's variables 'username' and 'password'
-print ('Logging in now through Reddit. If you get an error you may need to set your name and password in crewmate.py')
-driver.get("https://pixelplace.io/api/sso.php?type=2&action=login")
-driver.find_element_by_id('loginUsername').send_keys(crewmate.username)
-driver.find_element_by_id('loginPassword').send_keys(crewmate.password)
-driver.find_elements_by_xpath('/html/body/div/main/div[1]/div/div[2]/form/fieldset')[4].click()
-time.sleep(7)#adjust this sleep in seconds if logging after sending name/pass is slow for you
-driver.find_elements_by_xpath('/html/body/div[3]/div/div[2]/form/div/input')[0].click()
-print ('All done logging in, ready to paint.')
-#end login
-
-print ('-Controls-')
-print ('Q: Cycle through all colors.')
-print ('WASD: cycles different color palletes depending on which combination you use, for example W + A is yellows.')
-print ('E: Draw an Among Us character, make sure your zoom level is 1 on pixelplace. Note: it only works when you are moving your mouse cursor slighty when you press it.')
-print ('For random drawing, press Y on the top left of your zone, and U on the bottom left then use either R to draw random trees or K to draw random Among Us characters.')
-print ('J: hold J to stop drawing random trees or Among Us characters.')
-print ('ESC: hold Escape to halt the script.')
-
-#setting up the colors for fast color switching:
-def reload_colors(): #in order for you to switch colors easily, these needed to be loaded properly
-    global white,grey1,grey2,grey3,grey4,black,green1,green2,green3,green5,yellow1,yellow2,yellow3,yellow4,brown1,brown2,brown3,red1,red2,red3,brown4,peach1,peach2,peach3,pink1,pink2,pink3,pink4,blue1,blue2,blue3,blue4,blue5,blue6,blue7, colors_all, color0,  color1, color2, color3, color4, color5, color6, color7, colors_cycle0, colors_cycle1, colors_cycle2, colors_cycle3, colors_cycle4, colors_cycle5, colors_cycle6, colors_cycle7, colors_cycle8
-    white = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[0]
-    grey1 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[1]
-    grey2 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[2]
-    grey3 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[3]
-    grey4 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[4]
-    black = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[5]
-    green1 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[6]
-    green2 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[7]
-    green3 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[8]
-    green5 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[10]
-    yellow1 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[11]
-    yellow2 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[12]
-    yellow3 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[13]
-    yellow4 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[14]
-    brown1 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[15]
-    brown2 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[16]
-    brown3 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[17]
-    red1 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[18] 
-    red2 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[19]
-    red3 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[20]
-    brown4 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[22]
-    peach1 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[23]
-    peach2 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[24]
-    peach3 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[25]
-    pink1 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[26]
-    pink2 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[27]
-    pink3 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[28]
-    pink4 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[29]
-    blue1 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[31]
-    blue2 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[32]
-    blue3 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[33]
-    blue4 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[34]
-    blue5 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[35]
-    blue6 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[36]
-    blue7 = driver.find_elements_by_xpath('//*[@id="palette-buttons"]/a')[37]    
-    color8 = [white,grey1,grey2,grey3,grey4,black,green1,green2,
-        green3,green5,yellow1,yellow2,yellow3,yellow4,
-        brown1,brown2,brown3,red1,red2,red3,brown4,peach1,
-        peach2,peach3,pink1,pink2,pink3,pink4,
-        blue1,blue2,blue3,blue4,blue5,blue6,blue7]
-    #if you want to add colors to your WASD palette, add the colors into the lists below: 
-    color7 = [blue1,blue2,blue3,blue4,blue5,blue6,blue7]
-    color6 = [black, white]
-    color5 = [brown1, brown2, brown3, brown4, grey4, peach3, white]
-    color4 = [brown1, brown2, brown3, brown4]
-    color3 = [grey4, grey3, grey2, grey1]
-    color2 = [yellow1, yellow2, yellow3, yellow4]
-    color1 = [green1, green2, green3, green5]
-    color0 = [red1, red2, red3]
-    #the list above is linked to the whichcolor() functions further below
-    colors_cycle8 = cycle(color8)
-    colors_cycle7 = cycle(color7)
-    colors_cycle6 = cycle(color6)
-    colors_cycle5 = cycle(color5)
-    colors_cycle4 = cycle(color4)
-    colors_cycle3 = cycle(color3)
-    colors_cycle2 = cycle(color2)
-    colors_cycle1 = cycle(color1)
-    colors_cycle0 = cycle(color0)  
-reload_colors()
-
-def whichcolor8(): # All colors
-    global thecolor
-    thecolor = next(colors_cycle8)
-    thecolor.click() 
-
-def whichcolor7(): # Blue colors
-    global thecolor
-    thecolor = next(colors_cycle7)
-    thecolor.click()
-
-def whichcolor6(): # Black and white
-    global thecolor
-    thecolor = next(colors_cycle6)
-    thecolor.click()    
-
-def whichcolor5(): # Tree trunk colors
-    global thecolor
-    thecolor = next(colors_cycle5)
-    thecolor.click()    
-
-def whichcolor4(): # Brown colors
-    global thecolor
-    thecolor = next(colors_cycle4)
-    thecolor.click()    
-
-def whichcolor3(): # Grey colors
-    global thecolor
-    thecolor = next(colors_cycle3)
-    thecolor.click()    
-
-def whichcolor2(): # Yellow colors
-    global thecolor
-    thecolor = next(colors_cycle2)
-    thecolor.click()    
-
-def whichcolor1(): # Green colors
-    global thecolor
-    thecolor = next(colors_cycle1)
-    thecolor.click()    
-
-def whichcolor0(): # Red colors
-    global thecolor
-    thecolor = next(colors_cycle0)
-    thecolor.click()
-
-def rainbowbrush(): #this is how the key combos work, for example if you press A and W it will run whichcolor2() aka the yellow colors
-    if keyboard.is_pressed("s") == True or keyboard.is_pressed("a") == True or keyboard.is_pressed("w") == True or keyboard.is_pressed("d") == True:        
-        #only A
-        if keyboard.is_pressed("a") == True and keyboard.is_pressed("s") != True and keyboard.is_pressed("w") != True and keyboard.is_pressed("d") != True:
-            whichcolor1()
-        #only S
-        if keyboard.is_pressed("s") == True and keyboard.is_pressed("a") != True and keyboard.is_pressed("w") != True and keyboard.is_pressed("d") != True:
-            whichcolor0()    
-        #only AW
-        if keyboard.is_pressed("a") == True and keyboard.is_pressed("w") == True and keyboard.is_pressed("d") != True and keyboard.is_pressed("s") != True:
-            whichcolor2()
-        #only W
-        if keyboard.is_pressed("w") == True and keyboard.is_pressed("a") != True and keyboard.is_pressed("s") != True and keyboard.is_pressed("d") != True:
-            whichcolor3()
-        #only WD
-        if keyboard.is_pressed("w") == True and keyboard.is_pressed("d") == True and keyboard.is_pressed("s") != True and keyboard.is_pressed("a") != True:
-            whichcolor4()
-        #only D
-        if keyboard.is_pressed("d") == True and keyboard.is_pressed("s") != True and keyboard.is_pressed("a") != True and keyboard.is_pressed("w") != True:
-            whichcolor5()
-        #only DS
-        if keyboard.is_pressed("d") == True and keyboard.is_pressed("s") == True and keyboard.is_pressed("a") != True and keyboard.is_pressed("w") != True:
-            whichcolor6()
-        #only SA
-        if keyboard.is_pressed("s") == True and keyboard.is_pressed("a") == True and keyboard.is_pressed("d") != True and keyboard.is_pressed("w") != True:
-            whichcolor7()
             
-vectors = [1,2,3,4];vector_cycle = cycle(vectors)
+vectors = [1,2,3,4]
+vector_cycle = cycle(vectors)
+
 def vectoring():
     global vector #for use in direction of trees, among other things
     vector = next(vector_cycle)
@@ -375,7 +304,7 @@ def mongus(): #draw an among us character
     z = -z #next amongus will face other direction if you invert z
     
 ### Controls Section ###
-def mouse_handler(event):
+def mouse_handler():
     global x, y, z, tX, tY, bX, bY, pixx, pixx2
     x, y = autogui.position()
     if keyboard.is_pressed("q"):
